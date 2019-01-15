84f23bd7571e9a228bb21dfe3bfb40bc90b3976f
Paolo Rüegg <paolofalco@gmx.ch>
2019-01-15 16:01:54 +0000

    Added acceleration for the train

---
+++ unity/190115_HelloWorld/Assets/Scripts/TrainMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMove : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Accelerating...");
+            rigidBody.AddRelativeForce(Vector3.down);
+        }
+    }
+}
