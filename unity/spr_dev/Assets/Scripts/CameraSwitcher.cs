970beb9033c0e922d5105a6c5099f4d5d334e3fd
Paolo Rüegg <paolofalco@gmx.ch>
2019-02-08 17:09:44 +0000

    Added double platforms and double trains, changed environment etc.

--- unity/spr_dev/Assets/Scripts/CameraSwitcher.cs
+++ unity/spr_dev/Assets/Scripts/CameraSwitcher.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class CameraSwitcher : MonoBehaviour
 {
 
-    public Camera CameraMain, CameraAlt;
-    public bool camSwitch = false;
+    public Camera Camera_1, Camera_2, Camera_3;
+    public char activeCam = '0';
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.Alpha7))
         {
-            camSwitch = !camSwitch;
-            CameraMain.gameObject.SetActive(camSwitch);
-            CameraAlt.gameObject.SetActive(!camSwitch);
+            Camera_1.gameObject.SetActive(true);
+            Camera_2.gameObject.SetActive(false);
+            Camera_3.gameObject.SetActive(false);
+            activeCam = '1';
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            Camera_1.gameObject.SetActive(false);
+            Camera_2.gameObject.SetActive(true);
+            Camera_3.gameObject.SetActive(false);
+            activeCam = '2';
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            Camera_1.gameObject.SetActive(false);
+            Camera_2.gameObject.SetActive(false);
+            Camera_3.gameObject.SetActive(true);
+            activeCam = '3';
         }
     }
 }
--- unity/spr_dev/Assets/Scripts/SquareScaler.cs
+++ unity/spr_dev/Assets/Scripts/SquareScaler.cs
@@ -8,8 +8,11 @@ public class SquareScaler : MonoBehaviour
 
     public Transform train;
 
-    float z_pos = 100f;
 
+
+    float start_distance = 200;
+
+    public float SpeedSensitivity = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +22,23 @@ public class SquareScaler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        z_pos = train.transform.position.z; // Starts at 200
-        print(z_pos / 200f);
+        float z_pos = train.transform.position.z; // Starts at 200
 
-        transform.localScale = new Vector3(z_pos / 200f, z_pos / 200f, 1);
+        float gradient = 1 / ((1 / SpeedSensitivity) * start_distance);
+        float intercept = 1 - start_distance * gradient;
+        float new_scale = gradient * z_pos + intercept;
+
+
+        // = (z_pos / start_distance) / SpeedSensitivity;
+
+        transform.localScale = new Vector3(new_scale, new_scale, 1);
+
 
         if (Input.GetKey(KeyCode.Q))
         {
             transform.position = new Vector3(10, 10, 0);
         }
 
-        else if (Input.GetKey(KeyCode.E))
-        {
-        }
     }
+
 }
