850c2e6b4d2bdcf7db1ca4fc17cd7dd20781c73e
Paolo Rüegg <paolofalco@gmx.ch>
2019-03-10 16:15:21 +0000

    Moved scripts to right locations. Extended data collection to six scenarios.

--- unity/spr_dev/Assets/Scripts/S6/S6_SendToGoogle.cs
+++ unity/spr_dev/Assets/Scripts/S6/S6_SendToGoogle.cs
@@ -11,20 +11,21 @@ public class S6_SendToGoogle : MonoBehaviour
     public GameObject participantIDInput;
 
     private string participantID;
-    private float scenario1;
-    private float scenario2;
-    private float scenario3;
+    private float scenario1, scenario2, scenario3, scenario4, scenario5, scenario6;
 
     [SerializeField]
     private string BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdHeBv4TYwZJbrSugEQVgvvMGkVFy1IEn5On565OXhkoTFkXg/formResponse";
 
-    IEnumerator Post(string scenario1, string scenario2, string scenario3)
+    IEnumerator Post(string scenario1, string scenario2, string scenario3, string scenario4, string scenario5, string scenario6)
     {
         WWWForm form = new WWWForm();
 
         form.AddField("entry.850103797", scenario1);
-        form.AddField("entry.419789895", scenario1);
-        form.AddField("entry.803556472", scenario1);
+        form.AddField("entry.419789895", scenario2);
+        form.AddField("entry.803556472", scenario3);
+        form.AddField("entry.1000874584", scenario4);
+        form.AddField("entry.8764226", scenario5);
+        form.AddField("entry.1507321686", scenario6);
         byte[] rawData = form.data;
         WWW www = new WWW(BASE_URL, rawData);
         yield return www;
@@ -42,9 +43,12 @@ public class S6_SendToGoogle : MonoBehaviour
         scenario1 = responses.scenarioResponses[0];
         scenario2 = responses.scenarioResponses[1];
         scenario3 = responses.scenarioResponses[2];
+        scenario4 = responses.scenarioResponses[3];
+        scenario5 = responses.scenarioResponses[4];
+        scenario6 = responses.scenarioResponses[5];
 
-        Debug.Log("Information sent " + scenario1 + " " + scenario2 + " " + scenario3);
+        Debug.Log("Information sent " + scenario1 + " " + scenario2 + " " + scenario3 + " " + scenario4 + " " + scenario5 + " " + scenario6);
 
-        StartCoroutine(Post(scenario1.ToString(), scenario2.ToString(), scenario3.ToString()));
+        StartCoroutine(Post(scenario1.ToString(), scenario2.ToString(), scenario3.ToString(), scenario4.ToString(), scenario5.ToString(), scenario6.ToString()));
     }
 }
